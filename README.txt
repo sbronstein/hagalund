a9d17f374163e98de0577f903824d0fcc4d729a2
Stephen Bronstein <steve@bronstein.org>
2012-04-07 12:46:24 -0700

    initial commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+Hagalund is a very basic Pyramid + MongoDB app. It is starting as simply the result of generating an app with the Pyramid/MongoDB scaffolding. Plus this README of course. Never underestimate the README.
+
---
+++ hagalund/__init__.py
@@ -0,0 +1,33 @@
+from pyramid.config import Configurator
+from pyramid.events import subscriber
+from pyramid.events import NewRequest
+import pymongo
+
+from hagalund.resources import Root
+
+def main(global_config, **settings):
+    """ This function returns a WSGI application.
+    """
+    config = Configurator(settings=settings, root_factory=Root)
+    config.add_view('hagalund.views.my_view',
+                    context='hagalund:resources.Root',
+                    renderer='hagalund:templates/mytemplate.pt')
+    config.add_static_view('static', 'hagalund:static')
+    # MongoDB
+    def add_mongo_db(event):
+        settings = event.request.registry.settings
+        url = settings['mongodb.url']
+        db_name = settings['mongodb.db_name']
+        db = settings['mongodb_conn'][db_name]
+        event.request.db = db
+    db_uri = settings['mongodb.url']
+    MongoDB = pymongo.Connection
+    if 'pyramid_debugtoolbar' in set(settings.values()):
+        class MongoDB(pymongo.Connection):
+            def __html__(self):
+                return 'MongoDB: <b>{}></b>'.format(self)
+    conn = MongoDB(db_uri)
+    config.registry.settings['mongodb_conn'] = conn
+    config.add_subscriber(add_mongo_db, NewRequest)
+    config.scan('hagalund')
+    return config.make_wsgi_app()
