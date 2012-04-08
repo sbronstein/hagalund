85c9985605958c95831e4ef0eab444debda580a4
Stephen Bronstein <steve@bronstein.org>
2012-04-07 17:21:29 -0700

    removed page hit tracker

--- hagalund/views.py
+++ hagalund/views.py
@@ -1,5 +1,3 @@
 import datetime
 def my_view(request):
-    new_page_hit = {"timestamp":datetime.datetime.utcnow(), "url":request.url} 
-    request.db.page_hits.insert(new_page_hit, safe=True)
     return {'project':'hagalund'}
