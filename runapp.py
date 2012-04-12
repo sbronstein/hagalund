4b83014a12fef555b65627060e1ea962f22bafe3
Niall O'Higgins <niallo@unworkable.org>
2012-04-11 17:13:58 -0700

    port to pyramid 1.3

--- runapp.py
+++ runapp.py
@@ -1,10 +1,11 @@
 import os
 
-from paste.deploy import loadapp
 from waitress import serve
+from pyramid.paster import get_app
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app = loadapp('config:production.ini', relative_to='.')
+    app = get_app(os.path.join(os.path.dirname(__file__), 'production.ini'))
 
     serve(app, host='0.0.0.0', port=port)
