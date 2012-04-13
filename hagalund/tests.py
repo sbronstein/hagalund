04ab7a43da63279230e18c01672dc85fcc385425
Niall O'Higgins <niallo@unworkable.org>
2012-04-12 17:35:08 -0700

    update

--- hagalund/tests.py
+++ hagalund/tests.py
@@ -5,6 +5,13 @@ from pyramid import testing
 class ViewTests(unittest.TestCase):
     def setUp(self):
         self.config = testing.setUp()
+        from hagalund import main
+
+        app = main({}, **{"mako.directories":"dragpushpullcom:templates"})
+        from webtest import TestApp
+        self.testapp = TestApp(app)
+        self.settings = app.registry.settings
+        self.db = self.settings['mongodb_conn'][self.settings["mongodb.db_name"]]
 
     def tearDown(self):
         testing.tearDown()
--- hagalund/views.py
+++ hagalund/views.py
@@ -1,3 +1,5 @@
 import datetime
+
 def my_view(request):
+    request.db.page_hits.insert({"timestamp":datetime.datetime.utcnow()})
     return {'project':'hagalund'}
